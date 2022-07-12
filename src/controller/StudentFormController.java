169f5c41656c5aa8d33be075b3cf461c18097c06
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-07-12 10:51:24 +0530

    load all students with table

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -2,12 +2,17 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import model.Student;
 import util.SQLUtil;
 
@@ -38,6 +43,58 @@ public class StudentFormController {
     public TableColumn colAddress;
     public TableColumn colNic;
     public TableColumn colOperate;
+
+    public void initialize(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("stId"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("stName"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        colNic.setCellValueFactory(new PropertyValueFactory<>("nic"));
+        colOperate.setCellValueFactory(param -> {
+            ImageView edit = new ImageView("assets/edit.png");
+            ImageView delete = new ImageView("assets/delete.png");
+
+            HBox hBox = new HBox(edit, delete);
+            edit.setFitHeight(20);
+            edit.setFitWidth(20);
+            delete.setFitHeight(20);
+            delete.setFitWidth(20);
+            edit.setStyle("-fx-cursor: Hand");
+            delete.setStyle("-fx-cursor: Hand");
+
+            hBox.setStyle("-fx-alignment: Center");
+            HBox.setMargin(edit, new Insets(2,3,2,2));
+            HBox.setMargin(delete, new Insets(2,2,2,3));
+
+            return new ReadOnlyObjectWrapper<>(hBox);
+
+        });
+
+        try {
+            loadAllStudents();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAllStudents() throws SQLException, ClassNotFoundException {
+        ObservableList<Student> obList = FXCollections.observableArrayList();
+        ResultSet resultSet = SQLUtil.executeQuery("SELECT * FROM student");
+
+        while (resultSet.next()) {
+            obList.add(new Student(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getString(5),
+                    resultSet.getString(6)
+            ));
+        }
+        tblStudent.setItems(obList);
+    }
 
     public void newBtnOnAction(ActionEvent actionEvent) {
     }
