67707b0886a75251d3ee08d6526b90d4f8dccaf7
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-07-12 10:10:23 +0530

    refactored student management UI

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,9 +1,43 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 /**
  * @author : Gihan Madhusankha
  * 2022-07-12 9:44 AM
  **/
 
 public class DashboardFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtEmail;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public JFXTextField txtNic;
+    public JFXTextField txtAddress;
+    public JFXButton btnAdd;
+    public JFXButton btnClearForm;
+    public JFXButton btnNew;
+    public TextField txtSearch;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+    public TableColumn colOperate;
+
+    public void newBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void clearFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addBtnOnAction(ActionEvent actionEvent) {
+    }
 }
