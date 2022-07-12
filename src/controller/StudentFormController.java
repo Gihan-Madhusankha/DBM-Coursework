09dee2e4e6e6cf51552f39c05712373062925555
Gihan-Madhusankha <gmadusanka1234@gmail.com>
2022-07-12 10:30:25 +0530

    rename dashboardForm to studentForm

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -2,17 +2,24 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.Student;
+import util.SQLUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : Gihan Madhusankha
  * 2022-07-12 9:44 AM
  **/
 
-public class DashboardFormController {
+public class StudentFormController {
     public JFXTextField txtId;
     public JFXTextField txtEmail;
     public JFXTextField txtName;
@@ -23,7 +30,7 @@ public class DashboardFormController {
     public JFXButton btnClearForm;
     public JFXButton btnNew;
     public TextField txtSearch;
-    public TableView tblStudent;
+    public TableView<Student> tblStudent;
     public TableColumn colId;
     public TableColumn colName;
     public TableColumn colEmail;
